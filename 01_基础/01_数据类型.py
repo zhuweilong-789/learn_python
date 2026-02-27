69c20d817842ae0c18f7c62c38dd095683d7a2f2
zhuweilong <zhuweilong1127@qq.com>
2026-02-27 10:49:30 +0800

    feat：添加数据类型

---
+++ 01_基础/01_数据类型.py
@@ -0,0 +1,23 @@
+# 整数类型
+age = 18
+sum = 1_000_000  # 下划线可以用于分隔数字，提高可读性
+
+# 浮点数类型
+height = 1.75
+weight = 70.5
+
+# 字符串
+name = "张三"
+
+# 布尔类型
+'''
+布尔类型，只有 True 和 False 两个值
+运算符，
+    比较运算符：==, !=, >, <, >=, <=
+    逻辑运算符：and, or, not
+'''
+is_student = True
+is_girl = False
+
+# 空值
+none_value = None
