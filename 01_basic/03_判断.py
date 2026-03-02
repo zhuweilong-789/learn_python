a6fae4819ee64953b266b45396f580b51e0b9632
zhuweilong <zhuweilong1127@qq.com>
2026-03-02 12:34:38 +0800

    feat: 添加基础Python示例文件

---
+++ 01_basic/03_判断.py
@@ -0,0 +1,17 @@
+age = 18
+if age >= 18:
+    print('成年人')
+else:
+    print('未成年人')
+
+
+if age >= 18:
+    print('成年人')
+elif age >= 6:
+    print('小学生')
+else:
+    print('未成年人')
+
+# 简写
+if age:
+    print('成年人')
