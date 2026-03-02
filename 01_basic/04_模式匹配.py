4f937c62873e4809625401cd912ea4c95ad10da2
zhuweilong <zhuweilong1127@qq.com>
2026-03-02 17:33:09 +0800

    feat: 添加基础Python示例代码文件
    添加循环、字典和集合、函数定义以及模式匹配的基础示例代码文件

---
+++ 01_basic/04_模式匹配.py
@@ -0,0 +1,28 @@
+# 模式匹配
+space = 5
+
+match space:
+    case 1:
+        print('1个空格')
+    case 2:
+        print('2个空格')
+    case 3:
+        print('3个空格')
+    case _:  # 表示匹配到其他任何情况
+        print('其他空格')   
+
+
+'''match可以匹配多个值'''
+space = 5
+
+match space:
+    case x if x < 10: # 匹配小于10的空格
+        print(f'< 10 spaces: {x}')
+    case 10:
+        print('10 spaces.')
+    case 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18:
+        print('11~18 spaces.')
+    case 19:
+        print('19 spaces.')
+    case _:
+        print('其他空格')   
