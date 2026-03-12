300f50a871465b12e60303b6cda575fb4dfd3339
zhuweilong <zhuweilong1127@qq.com>
2026-03-12 14:46:50 +0800

    feat(函数): 添加函数定义和参数使用示例
    添加空函数示例、多返回值函数示例
    添加默认参数、可变参数和关键字可变参数的示例

--- 02_function/01_函数定义.py
+++ 02_function/01_函数定义.py
@@ -4,6 +4,20 @@ def welcome():
 
 welcome()
 
+# 定义一个空函数
+def empty_func():
+    # 相当于现在没想好函数的实现，先定义一个空函数
+    pass
+
+# python的函数可以返回多个值，但实际是返回了一个元组
+def func():
+    return 1, 2, 3
+
+a, b, c = func() 
+print(a, b, c) # 1 2 3
+t = func()
+print(t) # (1, 2, 3)
+
 
 
 
--- 02_function/02_函数参数的使用.py
+++ 02_function/02_函数参数的使用.py
@@ -13,3 +13,26 @@ def order_restrict(num, price, /, age, *, name):
     print(f"您的姓名是{name}，您的年龄是{age}岁")
 order_restrict(2, 10, 18, name="张三") # age可以是位置参数，也可以是关键字参数
 order_restrict(2, 10, age=18, name="张三")
+
+
+# 默认参数
+def order_default(num, price=10):
+    print(f"您点了{num}份商品，单价{price}元，总价{num * price}元")
+
+order_default(2) # 没有传price参数，默认使用10元
+order_default(2, 15) # 传了price参数，使用传进来的15元
+
+# 可变参数
+def order_var(num, *prices):
+    print(f"您点了{num}份商品，单价{prices}元，总价{num * sum(prices)}元")
+
+order_var(2, 10, 15) # 10和15是可变参数，传进来的参数会被打包成一个元组
+order_var(2, 10, 15, 20) # 10, 15, 20是可变参数，传进来的参数会被打包成一个元组
+
+
+ # 关键字可变参数
+def order_kwargs(num, **kwargs):
+    print(f"您点了{num}份商品，单价{kwargs}元，总价{num * sum(kwargs.values())}元")
+
+order_kwargs(2, a=10, b=15) # a=10, b=15是关键字可变参数，传进来的参数会被打包成一个字典
+order_kwargs(2, a=10, b=15, c=20) # a=10, b=15, c=20是关键字可变参数，传进来的参数会被打包成一个字典
