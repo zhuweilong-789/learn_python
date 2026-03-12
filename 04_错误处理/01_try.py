466f3847e643ea9ac1cc8631c5440a35959ea93f
zhuweilong <zhuweilong1127@qq.com>
2026-03-12 16:20:49 +0800

    fix(错误处理): 增强异常处理并添加错误日志记录
    在除法运算的异常处理中捕获异常对象以便调试
    添加日志记录功能来捕获和记录程序中的异常

--- 04_错误处理/01_try.py
+++ 04_错误处理/01_try.py
@@ -2,7 +2,7 @@
 
 try:
   r = 10 / 0
-except ZeroDivisionError:
+except ZeroDivisionError as e:
   print("除数不能为0")
 finally:
   print("finally")
@@ -24,5 +24,24 @@ def main():
 
 main()
 
-# BaseException 是所有错误的基类
+
+# 记录错误日志
+import logging
+
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+main()
+print('END')
+
+
 
